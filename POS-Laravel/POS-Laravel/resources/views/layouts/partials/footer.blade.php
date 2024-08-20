26cdf4b9506875b833c98d2142e3be94fb66b66b
rafia-khan <khanrafia9218@gmail.com>
2024-08-20 16:24:39 +0600

    up

--- POS-Laravel/POS-Laravel/resources/views/layouts/partials/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      Rafia
-    </div>
-    <strong>&copy; <?php echo date('Y')?> - Point of Sale System</strong> -
-  </footer>
