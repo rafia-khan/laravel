c7c22d12e534bf1e2c7ca71a99a775549898efde
rafia-khan <khanrafia9218@gmail.com>
2024-09-02 23:18:16 +0600

    update

--- one-to-one/routes/web.php
+++ one-to-one/routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
     return view('welcome');
 });
