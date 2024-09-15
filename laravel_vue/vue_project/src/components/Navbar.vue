96a06e02db689098233fa208534314ead7e5ad68
rafia-khan <khanrafia9218@gmail.com>
2024-09-15 17:57:49 +0600

    up

---
+++ laravel_vue/vue_project/src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <nav class="navbar">
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+      <li><router-link to="/services">Services</router-link></li>
+      <li><router-link to="/contact">Contact</router-link></li>
+      </ul>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: "Navbar",
+  };
+  </script>
+  
+  <style scoped>
+  .navbar {
+    background-color: #333;
+    padding: 1rem;
+  }
+  
+  .navbar ul {
+    list-style-type: none;
+    display: flex;
+    justify-content: space-around;
+  }
+  
+  .navbar li a {
+    color: white;
+    text-decoration: none;
+    padding: 0.5rem 1rem;
+  }
+  
+  .navbar li a:hover {
+    background-color: #555;
+  }
+  </style>
+  
---
+++ laravel_vue/vue_project/src/components/Sidebar.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="sidebar">
+      <ul>
+        <li><router-link to="/">Dashboard</router-link></li>
+      <li><router-link to="/profile">Profile</router-link></li>
+      <li><router-link to="/settings">Settings</router-link></li>
+      <li><router-link to="/logout">Logout</router-link></li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Sidebar',
+  };
+  </script>
+  
+  <style scoped>
+  .sidebar {
+    width: 250px;
+    height: 100vh;
+    background-color: #333;
+    padding-top: 20px;
+    position: fixed;
+    top: 0;
+    left: 0;
+  }
+  
+  .sidebar ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  .sidebar ul li {
+    margin: 20px 0;
+  }
+  
+  .sidebar ul li a {
+    color: white;
+    text-decoration: none;
+    padding: 10px;
+    display: block;
+  }
+  
+  .sidebar ul li a:hover {
+    background-color: #575757;
+  }
+  </style>
+  
