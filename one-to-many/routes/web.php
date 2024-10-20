5c3f3b2715b0daffb48e2e28ff7bee41b73b881e
rafia-khan <khanrafia9218@gmail.com>
2024-10-20 20:18:28 +0600

    update

--- one-to-many/routes/web.php
+++ one-to-many/routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\PostController;
 use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
+    return view('welcome');
     return view('welcome');
 });
 
